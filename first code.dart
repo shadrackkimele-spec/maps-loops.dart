e2aaa239a7a96c9e58b761aef2bbaadc8bb95487
2kay <shadrackkimele@gmail.com>
2026-07-29 19:58:14 +0300

    variables+list

--- first code.dart
+++ first code.dart
@@ -1,19 +1,20 @@
 void main(){
-String name="kim";
-String firstName="kim";
-int number=21;
-int myNumber=21;
-dynamic myName="kim";
-var age=21;
-double height=1.70;
-bool isRich=true;
-print("name: $name");
-print("firstName: $firstName");
-print("number: $number");
-print("myNumber: $myNumber");
-print("myName: $myName");
-print("age: $age");
-print("height: $height");
-print("isRich: $isRich");
-print("Nextyear i will be ${age+1} years old");
+    var name="kim";
+    int age=41;
+    double height=1.6;
+    bool isStudent=true;
+    dynamic myName="kim";
+    var emptyList=[];
+    emptyList.add("mango");
+    emptyList.addAll(["cake","honey"]);
+    print("name: $name");
+    print("int:$age");
+    print("double:$height");
+    print("bool:$isStudent");
+    print("dynamic:$myName");
+    print("Next year I will be ${age+2}years old");
+    print("emptyList:$emptyList");
+    print("emptyList[mango]");
+    print("emptyList[cake]");
+    print("emptyList[laptop,guitar]");
 }
