37cdeef908fa8473950ac174677655e09cc70db8
2kay <shadrackkimele@gmail.com>
2026-07-29 07:42:36 +0300

    initial commit.

---
+++ first code.dart
@@ -0,0 +1,13 @@
+void main(){
+var myName="kim";
+int age=23;
+double height=1.70;
+var myList=["1,2,3,4"];
+bool isStudent=true;
+print("myName:$myName");
+print("int:$age");
+print("double:$height");
+print("myList:$myList");
+print("bool:$isStudent");
+print("next year i will be ${age+1}");
+}
