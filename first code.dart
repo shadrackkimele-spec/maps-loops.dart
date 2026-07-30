80041b6ed01d5f5316c2ca24bb4049bb5a2ef0a6
2kay <shadrackkimele@gmail.com>
2026-07-30 22:41:47 +0300

    main

--- first code.dart
+++ first code.dart
@@ -1,20 +1,19 @@
 void main(){
     var name="kim";
-    int age=41;
-    double height=1.6;
-    bool isStudent=true;
-    dynamic myName="kim";
+    int age=22;
+    double weight=60.6;
+    bool isRich=true;
+    dynamic myName="kariuki";
     var emptyList=[];
-    emptyList.add("mango");
-    emptyList.addAll(["cake","honey"]);
-    print("name: $name");
-    print("int:$age");
-    print("double:$height");
-    print("bool:$isStudent");
-    print("dynamic:$myName");
-    print("Next year I will be ${age+2}years old");
+    emptyList.add(20);
+    emptyList.addAll([21,22,23]);
+    print("name:$name");
+    print("age:$age");
+    print("weight:$weight");
+    print("isRich:$isRich");
+    print("myName:$myName");
     print("emptyList:$emptyList");
-    print("emptyList[mango]");
-    print("emptyList[cake]");
-    print("emptyList[laptop,guitar]");
-}
+    print("emptyList[22]");
+    print("emptyList[60,61]");
+    print("Next year I will be ${age+1} years old");
+    }
